193c468472fbabd379180f1116153ae34b80ed9f
jpataflafla <jakub.kozlowski.mail@gmail.com>
2023-12-06 15:36:40 +0100

    day 6 - part 2

--- AdventOfCode2023/csharp/adventOfCode6/Program.cs
+++ AdventOfCode2023/csharp/adventOfCode6/Program.cs
@@ -7,20 +7,20 @@
 }
 
 /// <summary>
-/// Parses text in the format "Name: 1 2 3 4" into an array of integers.
+/// Parses text in the format "Name: 1 2 3 4" into an array of numbers.
 /// </summary>
 /// <param name="input">The input text to parse.</param>
-/// <returns>An array of integers parsed from the input text.</returns>
-int[] ParseToIntArray(string textLine)
+/// <returns>An array of numbers parsed from the input text.</returns>
+ulong[] ParseToNumberArray(string textLine)
 {
     string[] parts = textLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
     
     // Assuming the input format is "Name: 1 2 3 4", skipping the first element ("Name:")
-    int[] values = new int[parts.Length - 1];
+    ulong[] values = new ulong[parts.Length - 1];
     
-    for (int i = 1; i < parts.Length; i++)
+    for (var i = 1; i < parts.Length; i++)
     {
-        if (int.TryParse(parts[i], out int value))
+        if (ulong.TryParse(parts[i], out ulong value))
         {
             values[i - 1] = value;
         }
@@ -33,10 +33,10 @@ int[] ParseToIntArray(string textLine)
     return values;
 }
 
-int GetNumberOfWaysToWin(int raceTime, int recordDistance)
+ulong GetNumberOfWaysToWin(ulong raceTime, ulong recordDistance)
 {
-    var counterOfWins = 0;
-    int carChargingTime = raceTime - 1;
+    ulong counterOfWins = 0;
+    var carChargingTime = raceTime - 1;
     while(carChargingTime > 0 && carChargingTime < raceTime)
     {
         var currentDistance = (raceTime - carChargingTime) * carChargingTime;
@@ -49,19 +49,42 @@ int GetNumberOfWaysToWin(int raceTime, int recordDistance)
     return counterOfWins;
 }
 
+ulong MakeNumberFromIntArray(ulong[] array)
+{
+    if (array == null || array.Length == 0)
+    {
+        throw new ArgumentException("Array cannot be null or empty.");
+    }
+
+    // Concatenate the digits to form the number
+    string concatenatedDigits = string.Join("", array);
+
+    if (!ulong.TryParse(concatenatedDigits, out ulong result))
+    {
+        throw new FormatException($"Error parsing concatenated digits: {concatenatedDigits}");
+    }
+    return result;
+}
 
 var lines = GetLinesOfInput("input.txt");
 
-var times = ParseToIntArray(lines[0]);
+// Part 1
+var times = ParseToNumberArray(lines[0]);
 Console.WriteLine("Times: " + string.Join(' ', times));
-var records = ParseToIntArray(lines[1]);
+var records = ParseToNumberArray(lines[1]);
 Console.WriteLine("Records: " + string.Join(' ', records));
 
-var waysToWin = new int[times.Length];
-var combinationsOfWaysToWin = 1;
-for(int i = 0; i < waysToWin.Length; i++)
+ulong combinationsOfWaysToWin = 1;
+for(int i = 0; i < times.Length; i++)
 {
-    waysToWin[i] = GetNumberOfWaysToWin(times[i], records[i]);
-    combinationsOfWaysToWin *= waysToWin[i];
+    combinationsOfWaysToWin *= GetNumberOfWaysToWin(times[i], records[i]);
 }
 Console.WriteLine($"Combinations of ways to win: {combinationsOfWaysToWin}");
+
+// Part 2
+Console.WriteLine("Treat Times and Recors input as one Time int and one Record int (igonre whitespaces)");
+var numberOfWaysToWinOneRace = GetNumberOfWaysToWin(
+    MakeNumberFromIntArray(times),
+    MakeNumberFromIntArray(records));
+
+Console.WriteLine($"Combinations of ways to win one race (part 2 of the challange): {numberOfWaysToWinOneRace}");
